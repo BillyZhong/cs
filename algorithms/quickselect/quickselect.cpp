c59bdb7e44c7505030808fc90b1299b6c662568a
BillyZhong <billyczhong@gmail.com>
2015-11-11 16:26:19 -0800

    Quickselect finished

--- algorithms/quickselect/quickselect.cpp
+++ algorithms/quickselect/quickselect.cpp
@@ -2,10 +2,34 @@
 using namespace std;
 
 int p(int* a, int l, int h){
-  for(int i = l; i < h; i++){
-    
-  }
+  int t = 0;
+	int v = a[h];
+  int i = l;
+	for(int j = l; j < h; j++){
+		if(a[j] <= v){
+			t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+      i++;
+		}
+	}
+	t = a[i];
+	a[i] = a[h];
+	a[h] = t;
+	return i;
+}
 
+void q(int* a, int k, int l, int h){
+  int v = p(a, l, h);
+  if(k < v){
+    q(a, k, l, v-1);
+  }
+  else if(k == v){
+    cout << a[v] << endl;
+  }
+  else{
+    q(a, k, v+1, h);
+  }
 }
 
 int main(){
@@ -16,9 +40,5 @@ int main(){
   for(int i = 0; i < n; i++){
     cin >> a[i];
   }
-	p(a, k, 0, n);
-	for(int i = 0; i < n; i++){
-		cout << a[i] << ", ";
-	}
-	cout << endl;
+	q(a, k-1, 0, n-1);
 }
