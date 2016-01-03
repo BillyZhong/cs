ffdcb90917610a2b22693760ee4046fa5506daef
BillyZhong <billyczhong@gmail.com>
2016-01-03 15:49:19 -0800

    Linked Lists

---
+++ datastructures/linkedlist/README.md
@@ -0,0 +1,9 @@
+# Linked List
+* A *linked list* is a collection of nodes where each node has a value and a reference
+* **Singly-linked list**: nodes have pointers to the next node
+* **Doubly-linked list**: nodes have pointers to next and previous nodes
+* Time complexity:
+  * Access by index: `O(n)`
+  * Search by value: `O(n)`
+  * Insert: `O(1)`
+  * Delete: `O(1)`
---
+++ datastructures/linkedlist/implementation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+/**
+struct DoublyLinkedNode{
+  Node *next;
+  Node *prev;
+  int val;
+}
+**/
+
+struct Node {
+  Node *next;
+  int val;
+};
+
+Node * accessbyindex(int index, Node * head){
+  Node * temp = head;
+  for(int i = 1; i < index; i++){
+    temp = (*temp).next;
+  }
+  return temp;
+}
+
+Node * searchbyvalue(int query, Node * head){
+  Node * temp = head;
+  while((*temp).val != query){
+    temp = (*temp).next;
+  }
+  return temp;
+}
+
+void insertnode(Node * prev, Node * n){
+  (*n).next = (*prev).next;
+  (*prev).next = n;
+}
+
+void deletenode(Node * prev, Node * n){
+  (*prev).next = (*n).next;
+  delete n;
+}
+
+int main(){
+  Node * head = new Node;
+}
